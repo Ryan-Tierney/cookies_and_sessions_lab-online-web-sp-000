2cc63f7a2486d7292f82adca7ccdfa8b14ab3aeb
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-02 15:48:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def add_to_cart
-       
-     # Load the cart from the session, or create a new empty cart.
-    cart = session[:cart] || []
-       
-     # Save the cart in the session.
-    session[:cart] = cart
+  def cart
+    
   end
 end
